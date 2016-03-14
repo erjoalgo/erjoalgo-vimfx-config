beaaa0e807c8a28ffcd3bc44620bed9217895c0d
Ernesto Alfonso <ealfonso@debian-mini>
2016-03-13 18:55:22 -0700

    own config

--- VimFx-config@vimfx.org/config.js
+++ VimFx-config@vimfx.org/config.js
@@ -1,4 +1,41 @@
-// This is the actual config file, written in JavaScript. It is in this file you
-// add custom commands and set options, or whatever you'd like to do.
+vimfx.set('hint_chars', '1234qwerasdfgzxcvb')
 
-console.log('Hello, world! This is vimfx:', vimfx)
+let map = (shortcuts, command, custom=false) => {
+  vimfx.set(`${custom ? 'custom.' : ''}mode.normal.${command}`, shortcuts)
+}
+
+function disable_cmd ( cmd )	{
+    map("", cmd);
+}
+
+//disable a-prefixed cmds
+//todo write map_force to auto unbind conflicts
+[
+    "reload_all",
+    "reload_all_force", 
+    "stop_all", 
+    "find_highlight_all", 
+    "scroll_up", 
+    "scroll_down"
+].map(disable_cmd);
+
+
+
+map('a', 'scroll_to_left')
+map('s', 'scroll_to_right')
+map('d', 'scroll_half_page_down')
+map('w', 'scroll_half_page_up')
+
+map('A', 'scroll_left')
+map('S', 'scroll_right')
+
+
+map('f', 'follow')
+map('q', 'follow_in_tab')
+map('z', 'follow_multiple')
+map('.', 'scroll_to_bottom')
+map(',', 'scroll_to_top')
+
+map('<a-f1>', 'history_back')
+map('<a-f2>', 'history_forward')
+map('t', 'tab_new')
